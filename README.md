30da9bf69e5333f67e5d89b2c04599df1055d38c
Samuel Weibel <samuel.weibel@gmail.com>
2021-01-08 20:24:41 +0100

    Initial version of the driver.

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # dmx-contrib-anyma
-Driver for NodeJS dmx module supporting Anyma uDMX
+
+Driver for NodeJS [dmx](https://www.npmjs.com/package/dmx) module supporting Anyma uDMX.
+
+## Requirements
+
+You need to have an Anyma uDMX compatible USB adapter. Buy one at one of the following sources:
+
+- [Official Anyma Store](http://www.anyma.ch/store/11-udmx.html)
+- [Clones found on Aliexpress](https://www.aliexpress.com/wholesale?SearchText=USB+dmx)
+
+_IMPORTANT_ The author of this driver is not related to any of the above companies and does not take any responsibility for the proper working of the hardware bought at those links!
+
+## Usage
+
+To use this driver with the [dmx](https://www.npmjs.com/package/dmx) module, you need to register it as a driver:
+
+```javascript
+// load the DMX module
+const DMX = require('dmx');
+
+// register this driver module
+dmx.registerDriver('anyma', require('dmx-contrib-anyma'));
+```
+
+To add a universe using the driver, you need to call:
+
+```javascript
+dmx.addUniverse(
+  'my-device', // this can be an arbitrary string
+  'anyma', // this must be the same string as used in registerDriver() above
+  device_id, // (optional) see below for possible values
+  options, // (optional) see below for possible options
+);
+```
+
+The following values are possible for `device_id`:
+
+- `undefined`: the first Anyma device found will be used
+- `3`: the fourth (indexed from zero) Anyma device found will be used
+- `{ busNumber: 1, deviceAddress: 5 }`: the device at bus number 1 and with address 5 will be used
+
+The following properties are used from the `options` object:
+
+- `dmx_speed`: updates per second (in Hz), default: `33` (= 30 ms refresh interval)
---
+++ index.js
@@ -0,0 +1,81 @@
+const EventEmitter = require('events').EventEmitter;
+const usb = require('usb');
+
+// These constants specify the Anyma uDMX device
+const VENDOR_ID = 0x16c0;
+const PRODUCT_ID = 0x5dc;
+
+const UNIVERSE_LEN = 512;
+
+function Anyma(deviceId = undefined, options = {}) {
+  this.universe = Buffer.alloc(UNIVERSE_LEN + 1);
+  this.readyToWrite = true;
+  this.interval = 1000 / (options.dmx_speed || 33);
+
+  if (!deviceId) {
+    this.device = usb.findByIds(VENDOR_ID, PRODUCT_ID);
+  } else {
+    const devices = usb
+      .getDeviceList()
+      .filter((d) => d.deviceDescriptor.idVendor === VENDOR_ID && d.deviceDescriptor.idProduct === PRODUCT_ID);
+    if (deviceId.hasOwnProperty('busNumber') && deviceId.hasOwnProperty('deviceAddress')) {
+      this.device = devices.find((d) => d.busNumber == deviceId.busNumber && d.deviceAddress == deviceId.deviceAddress);
+    } else {
+      this.device = devices[deviceId];
+    }
+  }
+  if (!this.device) {
+    throw new Error(`Couldn't find Anyma uDMX dongle for deviceId=${JSON.stringify(deviceId)}`);
+  }
+  this.device.open();
+}
+
+Anyma.prototype.start = function () {
+  this.timeout = setInterval(() => {
+    this.sendUniverse();
+  }, this.interval);
+};
+
+Anyma.prototype.stop = function () {
+  clearInterval(this.timeout);
+};
+
+Anyma.prototype.close = (cb) => {
+  this.device.close();
+  cb(null);
+};
+
+Anyma.prototype.update = function (u, extraData) {
+  for (const c in u) {
+    this.universe[c] = u[c];
+  }
+
+  this.emit('update', u, extraData);
+};
+
+Anyma.prototype.updateAll = function (v, _) {
+  for (let i = 1; i <= UNIVERSE_LEN; i++) {
+    this.universe[i] = v;
+  }
+};
+
+Anyma.prototype.get = function (c) {
+  return this.universe[c];
+};
+
+Anyma.prototype.sendUniverse = function () {
+  if (!this.readyToWrite) {
+    return;
+  }
+  this.readyToWrite = false;
+  this.device.controlTransfer(0x40, 0x0002, UNIVERSE_LEN, 1, this.universe, function (error) {
+    this.readyToWrite = true;
+    if (error) {
+      console.warn(error);
+    }
+  });
+};
+
+util.inherits(Anyma, EventEmitter);
+
+module.exports = Anyma;
